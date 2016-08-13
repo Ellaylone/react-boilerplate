d4acd317f06c92c2edbac0703e8eccb695be84d4
ellaylone <heamik91@yandex.ru>
2016-08-13 18:40:30 +0300

    basic package.json & webpack config

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+let path = require('path')
+let webpack = require('webpack')
+
+module.exports = {
+    devtool: 'cheap-eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client',
+        './src/index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ]
+}
